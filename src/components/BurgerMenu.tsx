d2229bf761cbe92d4ed2905e769a446152b82405
Frisdahl <afrisser3@gmail.com>
2025-03-09 18:21:32 +0100

    changed font weight

--- src/components/BurgerMenu.tsx
+++ src/components/BurgerMenu.tsx
@@ -3,19 +3,19 @@ import { UnorderedList, ListItem, Link } from "@chakra-ui/react";
 
 const BurgerMenu = () => {
   return (
-    <UnorderedList styleType="none">
+    <UnorderedList styleType="none" fontWeight={"500"}>
       <ListItem mb={2}>
-        <Link href="#" fontSize={"1.25em"} fontWeight={"500"} color="white">
+        <Link href="#" fontSize={"1.25em"} color="white">
           Film
         </Link>
       </ListItem>
       <ListItem mb={2}>
-        <Link href="#" fontSize={"1.25em"} fontWeight={"500"} color="white">
+        <Link href="#" fontSize={"1.25em"} color="white">
           TV-Serier
         </Link>
       </ListItem>
       <ListItem mb={4}>
-        <Link href="#" fontSize={"1.25em"} fontWeight={"500"} color="white">
+        <Link href="#" fontSize={"1.25em"} color="white">
           Personer
         </Link>
       </ListItem>
