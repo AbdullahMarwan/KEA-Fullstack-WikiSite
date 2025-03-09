750c624dfb39a9bbac4fb28495091e2b3d5fb256
Frisdahl <afrisser3@gmail.com>
2025-03-09 17:38:16 +0100

    added burgerMenu and links. (has effect when clicked)

---
+++ src/components/BurgerMenu.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { UnorderedList, ListItem, Link } from "@chakra-ui/react";
+
+const BurgerMenu = () => {
+  return (
+    <UnorderedList styleType="none">
+      <ListItem mb={2}>
+        <Link href="#" fontSize={"1.25em"} fontWeight={"500"} color="white">
+          Film
+        </Link>
+      </ListItem>
+      <ListItem mb={2}>
+        <Link href="#" fontSize={"1.25em"} fontWeight={"500"} color="white">
+          TV-Serier
+        </Link>
+      </ListItem>
+      <ListItem mb={4}>
+        <Link href="#" fontSize={"1.25em"} fontWeight={"500"} color="white">
+          Personer
+        </Link>
+      </ListItem>
+      <ListItem mb={2}>
+        <Link href="#" color="#A0AEC0">
+          Bidragsbibel
+        </Link>
+      </ListItem>
+      <ListItem mb={2}>
+        <Link href="#" color="#A0AEC0">
+          Diskussioner
+        </Link>
+      </ListItem>
+      <ListItem mb={2}>
+        <Link href="#" color="#A0AEC0">
+          Leaderboard
+        </Link>
+      </ListItem>
+      <ListItem mb={2}>
+        <Link href="#" color="#A0AEC0">
+          API
+        </Link>
+      </ListItem>
+      <ListItem mb={2}>
+        <Link href="#" color="#A0AEC0">
+          Support
+        </Link>
+      </ListItem>
+      <ListItem mb={2}>
+        <Link href="#" color="#A0AEC0">
+          Om
+        </Link>
+      </ListItem>
+      <ListItem mb={2}>
+        <Link href="#" color="#A0AEC0">
+          Log ind
+        </Link>
+      </ListItem>
+    </UnorderedList>
+  );
+};
+
+export default BurgerMenu;
