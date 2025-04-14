453d11b86492d3c164e0c226b515231a8a8d830f
AbdullahMarwan <abdu.moussa01@gmail.com>
2025-04-14 11:13:21 +0200

    WIP Displaying Cards in a grid

--- client/src/pages/MoviesSubPage.tsx
+++ client/src/pages/MoviesSubPage.tsx
@@ -16,12 +16,13 @@ import {
   Wrap,
   WrapItem,
   Button,
+  Grid,
+  GridItem,
 } from "@chakra-ui/react";
 import Cards from "../components/Homepage/Cards";
 import {
   fetchTrendingMovies,
   fetchPopularMovies,
-  fetchStreamingMovies,
   fetchNowPlayingMovies,
   fetchTopRatedMovies,
   fetchUpcomingMovies,
@@ -64,121 +65,161 @@ const MoviesSubPage = () => {
   };
 
   return (
-    <Box p={4}>
-      <Heading mb={4}>{getCategoryHeading()}</Heading>
-      <Box
-        borderWidth="1px"
-        borderRadius="lg"
-        p={4}
-        bg="white"
-        boxShadow="md"
-        maxWidth="300px"
+    <Box padding="20px">
+      <Grid
+        templateAreas={{
+          base: `"header" 
+                 "aside" 
+                 "main"`, // Stack vertically on mobile
+          md: `"header header" 
+               "aside main"`, // Side by side on medium screens and up
+        }}
+        gridTemplateColumns={{
+          base: "1fr", // Full width single column on mobile
+          md: "300px 1fr", // Fixed width aside, flexible main on medium screens and up
+        }}
+        width="100%"
+        maxWidth="1300px"
+        margin="0 auto"
+        columnGap={6} // Add gap only between columns (side-to-side)
       >
-        {/* Sort Section */}
-        <Box mb={4}>
-          <Heading size="sm" mb={2}>
-            Sort
-          </Heading>
-          <Select placeholder="Sort Results By">
-            <option value="popularity.desc">Popularity Descending</option>
-            <option value="popularity.asc">Popularity Ascending</option>
-            <option value="release_date.desc">Release Date Descending</option>
-            <option value="release_date.asc">Release Date Ascending</option>
-          </Select>
-        </Box>
+        {/* Aside Section */}
+        <GridItem area={"aside"}>
+          <Box
+            borderWidth="1px"
+            borderRadius="lg"
+            p={4}
+            bg="white"
+            boxShadow="md"
+          >
+            <Heading size="md" mb={4}>
+              {getCategoryHeading()}
+            </Heading>
 
-        {/* Filters Section */}
-        <Accordion allowToggle>
-          {/* Show Me Section */}
-          <AccordionItem>
-            <AccordionButton>
-              <Box flex="1" textAlign="left">
-                Filters
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-            <AccordionPanel pb={4}>
-              {/* Show Me */}
+            {/* Sort Section */}
+            <Box mb={4}>
               <Heading size="sm" mb={2}>
-                Show Me
+                Sort
               </Heading>
-              <RadioGroup defaultValue="everything">
-                <Stack direction="column">
-                  <Radio value="everything">Everything</Radio>
-                  <Radio value="not-seen">Movies I Haven't Seen</Radio>
-                  <Radio value="seen">Movies I Have Seen</Radio>
-                </Stack>
-              </RadioGroup>
+              <Select placeholder="Sort Results By">
+                <option value="popularity.desc">Popularity Descending</option>
+                <option value="popularity.asc">Popularity Ascending</option>
+                <option value="release_date.desc">Release Date Descending</option>
+                <option value="release_date.asc">Release Date Ascending</option>
+              </Select>
+            </Box>
 
-              {/* Availabilities */}
-              <Heading size="sm" mt={4} mb={2}>
-                Availabilities
-              </Heading>
-              <Checkbox defaultChecked>Search all availabilities?</Checkbox>
+            {/* Filters Section */}
+            <Accordion allowToggle>
+              <AccordionItem>
+                <AccordionButton>
+                  <Box flex="1" textAlign="left">
+                    Filters
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+                <AccordionPanel pb={4}>
+                  {/* Show Me */}
+                  <Heading size="sm" mb={2}>
+                    Show Me
+                  </Heading>
+                  <RadioGroup defaultValue="everything">
+                    <Stack direction="column">
+                      <Radio value="everything">Everything</Radio>
+                      <Radio value="not-seen">Movies I Haven't Seen</Radio>
+                      <Radio value="seen">Movies I Have Seen</Radio>
+                    </Stack>
+                  </RadioGroup>
 
-              {/* Release Dates */}
-              <Heading size="sm" mt={4} mb={2}>
-                Release Dates
-              </Heading>
-              <Checkbox defaultChecked>Search all releases?</Checkbox>
-              <Stack direction="row" mt={2}>
-                <Input type="date" placeholder="From" />
-                <Input type="date" placeholder="To" />
-              </Stack>
+                  {/* Availabilities */}
+                  <Heading size="sm" mt={4} mb={2}>
+                    Availabilities
+                  </Heading>
+                  <Checkbox defaultChecked>Search all availabilities?</Checkbox>
 
-              {/* Genres */}
-              <Heading size="sm" mt={4} mb={2}>
-                Genres
-              </Heading>
-              <Wrap>
-                {[
-                  "Action",
-                  "Adventure",
-                  "Animation",
-                  "Comedy",
-                  "Crime",
-                  "Documentary",
-                  "Drama",
-                  "Family",
-                  "Fantasy",
-                  "History",
-                  "Horror",
-                  "Music",
-                  "Mystery",
-                  "Romance",
-                  "Science Fiction",
-                  "TV Movie",
-                  "Thriller",
-                  "War",
-                  "Western",
-                ].map((genre) => (
-                  <WrapItem key={genre}>
-                    <Button
-                      size="sm"
-                      variant="outline"
-                      colorScheme="blue"
-                      _hover={{ bg: "blue.100" }}
-                    >
-                      {genre}
-                    </Button>
-                  </WrapItem>
-                ))}
-              </Wrap>
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
-      </Box>
-      <Cards
-        fetchFunction={getFetchFunction()}
-        maxItems={10}
-        title="Trending"
-        showLinkSelector={true}
-        links={[
-          { name: "I dag", href: "#", value: category }, // Use the category variable here
-          { name: "Denne uge", href: "#", value: "week" },
-        ]}
-        defaultTimeWindow={defaultTimeWindow} // Use the defaultTimeWindow variable here as well
-      />
+                  {/* Release Dates */}
+                  <Heading size="sm" mt={4} mb={2}>
+                    Release Dates
+                  </Heading>
+                  <Checkbox defaultChecked>Search all releases?</Checkbox>
+                  <Stack direction="row" mt={2}>
+                    <Input type="date" placeholder="From" />
+                    <Input type="date" placeholder="To" />
+                  </Stack>
+
+                  {/* Genres */}
+                  <Heading size="sm" mt={4} mb={2}>
+                    Genres
+                  </Heading>
+                  <Wrap>
+                    {[
+                      "Action",
+                      "Adventure",
+                      "Animation",
+                      "Comedy",
+                      "Crime",
+                      "Documentary",
+                      "Drama",
+                      "Family",
+                      "Fantasy",
+                      "History",
+                      "Horror",
+                      "Music",
+                      "Mystery",
+                      "Romance",
+                      "Science Fiction",
+                      "TV Movie",
+                      "Thriller",
+                      "War",
+                      "Western",
+                    ].map((genre) => (
+                      <WrapItem key={genre}>
+                        <Button
+                          size="sm"
+                          variant="outline"
+                          colorScheme="blue"
+                          _hover={{ bg: "blue.100" }}
+                        >
+                          {genre}
+                        </Button>
+                      </WrapItem>
+                    ))}
+                  </Wrap>
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+          </Box>
+        </GridItem>
+
+        {/* Main Section */}
+        <GridItem area={"main"}>
+          <Box>
+            <Heading size="lg" mb={4}>
+              {getCategoryHeading()}
+            </Heading>
+            <Grid
+              templateColumns={{
+                base: "repeat(2, 1fr)", // 2 cards per row on small screens
+                md: "repeat(3, 1fr)", // 3 cards per row on medium screens
+                lg: "repeat(4, 1fr)", // 4 cards per row on large screens
+              }}
+              gap={6} // Space between cards
+            >
+              <Cards
+                fetchFunction={getFetchFunction()}
+                maxItems={10}
+                title={getCategoryHeading()}
+                showLinkSelector={true}
+                links={[
+                  { name: "I dag", href: "#", value: category },
+                  { name: "Denne uge", href: "#", value: "week" },
+                ]}
+                defaultTimeWindow={defaultTimeWindow}
+              />
+            </Grid>
+          </Box>
+        </GridItem>
+      </Grid>
     </Box>
   );
 };
