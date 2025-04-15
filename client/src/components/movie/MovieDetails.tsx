ebb24693a0dff0a1a33b97427e971dd5bbbc0c00
Frisdahl <afrisser3@gmail.com>
2025-04-15 15:44:45 +0200

    WIP

--- client/src/components/movie/MovieDetails.tsx
+++ client/src/components/movie/MovieDetails.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import {fetchTrendingMovies} from "../../services/api"; // Adjust the import path as necessary
 
 const MovieDetails = () => {
   const { id } = useParams(); // This grabs the `id` from the URL
@@ -8,9 +9,7 @@ const MovieDetails = () => {
   useEffect(() => {
     const fetchMovie = async () => {
       try {
-        const response = await fetch(
-          `https://api.themoviedb.org/3/movie/${id}?api_key=YOUR_API_KEY`
-        );
+        const response = await fetch (fetchTrendingMovies(id));
         const data = await response.json();
         console.log(data);
         setMovie(data);
