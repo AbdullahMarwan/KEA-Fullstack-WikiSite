3e4659da3a0b7f37ba5a6b8d6f02feef9a5111c7
AbdullahMarwan <abdu.moussa01@gmail.com>
2025-05-05 12:59:05 +0200

    Deleted unused components

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,10 +1,7 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import SearchBar from "../components/Homepage/SearchBar";
 import SearchMovieSection from "../components/Homepage/SearchMovieSection";
-import TrendingMovies from "../components/Homepage/TrendingMovies";
 import LatestTrailers from "../components/Homepage/LatestTrailers";
-import FilmSection from "../components/Homepage/FilmSection";
-import TvShows from "../components/Homepage/TvShows";
 import SignupSection from "../components/Signup/SignupSection";
 import MovieSection from "../components/Homepage/MovieSection";
 
@@ -22,13 +19,10 @@ export const Homepage = () => {
 
       <GridItem area={"main"}>
         <SearchMovieSection />
-        {/* <TrendingMovies /> */}
         <MovieSection sectionType="trending" />
         <LatestTrailers />
-        {/* <FilmSection /> */}
         <MovieSection sectionType="popular" />
         <SignupSection />
-        {/* <TvShows /> */}
         <MovieSection sectionType="tv-series" />
       </GridItem>
     </Grid>
--- client/src/pages/MoviesSubPage.tsx
+++ client/src/pages/MoviesSubPage.tsx
@@ -73,7 +73,7 @@ const MoviesSubPage = () => {
       case "top-rated":
         return fetchTemplate("", "top-rated");
       default:
-        return fetchTemplate("", "trending");
+        return fetchTemplate("day", "trending");
     }
   };
 
