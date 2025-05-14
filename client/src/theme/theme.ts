f8dbb0a1a2ff66d8764baad31c05bf8769bd70d1
Daniel Wirenfeldt <wirenfeldtd@gmail.com>
2025-05-14 14:36:44 +0200

    changed-theme-breakpoint-sizes

--- client/src/theme/theme.ts
+++ client/src/theme/theme.ts
@@ -6,6 +6,14 @@ import "@fontsource/source-sans-pro/400.css";
 import "@fontsource/source-sans-pro/600.css";
 import "@fontsource/source-sans-pro/700.css";
 
+const breakpoints = {
+  sm: "20em",   // 320px
+  md: "48em",   // 768px
+  lg: "62em",   // 992px
+  xl: "80em",   // 1280px
+  "2xl": "96em" // 1536px
+};
+
 const theme = extendTheme({
   fonts: {
     heading: `'Source Sans Pro', sans-serif`,
@@ -29,6 +37,7 @@ const theme = extendTheme({
       tertiary: "#1ed5aa",
     },
   },
+  breakpoints, // 👈 merged in here
 });
 
 export default theme;
