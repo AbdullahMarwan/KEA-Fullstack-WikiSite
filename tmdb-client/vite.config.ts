3a2514ea17e82d49ab7753d054fdf2d1f44f6a36
Daniel Wirenfeldt <wirenfeldtd@gmail.com>
2025-06-16 21:53:26 +0200

    added-url-to-config

--- tmdb-client/vite.config.ts
+++ tmdb-client/vite.config.ts
@@ -7,5 +7,12 @@ export default defineConfig({
   server: {
     port: 3000,
     host: "0.0.0.0",
+    cors: {
+      origin: [
+        'http://localhost:3000',
+        'https://tmdb-client-qoa6.onrender.com',
+      ],
+      credentials: true,
+    },
   },
 });
