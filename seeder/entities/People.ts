837c6d482cefdbf11f56084bcf33017461b2563a
Frisdahl <afrisser3@gmail.com>
2025-06-16 20:20:42 +0200

    added seeder for people and entites for tables, also removed force seeder in docker compose and made seeder check

---
+++ seeder/entities/People.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity({ name: "people" })
+export class People {
+  @PrimaryGeneratedColumn("increment", { type: "bigint" })
+  id!: number;
+
+  @Column({ type: "varchar", length: 100, nullable: true })
+  name!: string;
+
+  @Column({ name: "known_for", type: "varchar", length: 100, nullable: true })
+  known_for!: string;
+
+  @Column({ type: "tinyint", nullable: true })
+  gender!: number;
+
+  // Changed from timestamp to date to support older dates
+  @Column({ type: "date", nullable: true })
+  birthday!: Date | null;
+
+  @Column({ type: "varchar", length: 200, nullable: true })
+  biography!: string;
+}
