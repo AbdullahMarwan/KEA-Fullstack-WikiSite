326f63ff260c4f5d667564b17c10efe53749d7f8
Frisdahl <afrisser3@gmail.com>
2025-06-17 13:40:41 +0200

    added acces for vite.config

--- tmdb-client/vite.config.ts
+++ tmdb-client/vite.config.ts
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", // ✅ ADD THIS
   plugins: [react()],
   server: {
     port: 3000,
     host: "0.0.0.0",
     cors: {
       origin: [
-        'http://localhost:3000',
-        'https://tmdb-client-qoa6.onrender.com',
+        "http://localhost:3000",
+        "https://tmdb-client-qoa6.onrender.com",
+        "https://fullstack-client-mcdp.onrender.com", // ✅ Add this
       ],
       credentials: true,
     },
     allowedHosts: [
-      'tmdb-client-qoa6.onrender.com',
+      "tmdb-client-qoa6.onrender.com",
+      "fullstack-client-mcdp.onrender.com", // ✅ Add this
     ],
   },
 });
