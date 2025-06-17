45970e6ba4a5b8670ad789ce7875e7eb994560df
Frisdahl <afrisser3@gmail.com>
2025-06-17 14:30:39 +0200

    checking local and render

--- tmdb-server/startup/data-source.ts
+++ tmdb-server/startup/data-source.ts
@@ -4,13 +4,14 @@ import { User } from "../entities/User";
 import { Content } from "../entities/Content";
 import { People } from "../entities/People";
 
-const isRemoteDB = process.env.MYSQL_URL?.includes("aivencloud");
+const mysqlUrl = process.env.DATABASE_URL;
+const isRemoteDB = mysqlUrl?.includes("aivencloud");
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  url: process.env.MYSQL_URL,
+  url: mysqlUrl,
   synchronize: true,
   logging: false,
   entities: [User, Content, People],
-  ssl: isRemoteDB ? { rejectUnauthorized: false } : undefined, // 👈 dynamisk ssl
+  ssl: isRemoteDB ? { rejectUnauthorized: false } : undefined,
 });
